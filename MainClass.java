09a88b0d708b540966b3d215be3b8088b7def440
cleyton27 <cleyton27mb@gmail.com>
2019-07-01 08:50:58 -0400

    First class java about packages and your importance when you're starting to build a project. Packages also be considered containers for classes, but they actually define where classes will be localed in the hierarchical directory structure.

---
+++ MainClass.java
@@ -0,0 +1,29 @@
+
+//Package Statement
+package com.ocaj.exam.tutorial.chapter1;
+
+/* Imports class ArrayLis from  the java.util package */
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" \nEnter your favoriteplanet: ");
+        String planet =  sc.next();
+
+        /*Creat list for planets*/
+
+        ArrayList planetList = new ArrayList();
+        /* Adds users input to the list*/
+        planetList.add(planet);
+        //Adds a string to the list
+        planetList.add("Gliese 581 c");
+
+        System.out.println("\nTwo cool planets: "+ planetList);
+
+    }
+
+}
