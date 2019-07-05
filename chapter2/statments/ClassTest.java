0829e8c35b421c826b62fce936a0410f800a52b9
cleyton27 <cleyton27mb@gmail.com>
2019-07-05 11:03:21 -0400

    Learning Conditional Statements If and Else

---
+++ chapter2/statments/ClassTest.java
@@ -0,0 +1,12 @@
+package com.ocaj.exam.tutorial.chapter1.chapter2.statments;
+
+public class ClassTest {
+    public static void main(String[] args) {
+
+        IfElse ifElse = new IfElse();
+
+        ifElse.trueOrFalse();
+
+    }
+
+}
