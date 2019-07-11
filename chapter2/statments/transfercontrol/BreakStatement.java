625c481265063b9b09899ba9c881b9015ef0eeb8
cleyton27 <cleyton27mb@gmail.com>
2019-07-11 10:08:04 -0400

    adding comments in the method fishingByHour

--- chapter2/statments/transfercontrol/BreakStatement.java
+++ chapter2/statments/transfercontrol/BreakStatement.java
@@ -1,7 +1,9 @@
 package com.ocaj.exam.tutorial.chapter2.statments.transfercontrol;
 
 public class BreakStatement {
-
+   /*This method will stop to run when the when the number of hours allowed to fish
+   is reached
+    */
     public void fishingByHour(){
         int totaltHoursFishing = 0;
         int hoursAllowedFishing =4;
