c7774c9ce308adaed89fa350b216821834eefdf0
cleyton27 <cleyton27mb@gmail.com>
2019-07-11 11:04:09 -0400

    The continue statement is used  to terminate the current iteration of iteration statements and continue with the next iteration

--- chapter2/statments/transfercontrol/ContinueStatement.java
+++ chapter2/statments/transfercontrol/ContinueStatement.java
@@ -2,4 +2,36 @@ package com.ocaj.exam.tutorial.chapter2.statments.transfercontrol;
 
 public class ContinueStatement {
 
+    /*This method prints the days that's allowed to fish. When isn't allowed
+    to fish the method just skip fishing's days and prints only the camping
+
+     */
+    public void activetiesByDay(){
+    int totalDaysCamping =0;
+    int daysAllowedFish = 5;
+
+    for (int i= 1; i<8; i++){
+        totalDaysCamping++;
+        System.out.print("\nDay "+i +" :camping");
+
+        /*
+        Just print the fishing days that aren't between 1 and 5
+
+        if (totalDaysCamping >=1 && 5>= totalDaysCamping){
+            continue;
+        }*/
+
+        if (totalDaysCamping>daysAllowedFish){
+            continue;
+        }
+
+        System.out.print("and fishing");
+    }
+
+    }
+
+    public static void main(String[] args) {
+        ContinueStatement cs = new ContinueStatement();
+        cs.activetiesByDay();
+    }
 }
