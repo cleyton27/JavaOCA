e3e1c0c945d36b450d254101b945a8eeabf27677
cleyton27 <cleyton27mb@gmail.com>
2019-07-17 09:48:21 -0400

    Prefix Increment, Postfix Increment, Prefix decrement and Posfix decrement operators

---
+++ chapter3/PrefixAndPostfixIncrementAndDecrementOperators.java
@@ -0,0 +1,29 @@
+package com.ocaj.exam.tutorial.chapter3;
+
+public class PrefixAndPostfixIncrementAndDecrementOperators {
+    public static void main(String[] args) {
+        int x, y = 0;
+
+        //prefix operator
+
+        System.out.println("Prefix operators");
+        x =10;
+        y = ++x;
+        System.out.println("x :"+x +" y: "+y);
+
+        x =10;
+        y = --x;
+        System.out.println("x :"+x +" y: "+y);
+        System.out.println();
+        //postfix operator
+
+        System.out.println("Postfix operators");
+        x= 10;
+        y = x++;
+        System.out.println("x :"+x +" y: "+y);x= 10;
+
+        x= 10;
+        y = x--;
+        System.out.println("x :"+x +" y: "+y);
+    }
+}
