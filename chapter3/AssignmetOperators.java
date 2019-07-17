62afa2a58b69dc51cde630b27df323a997be3403
cleyton27 <cleyton27mb@gmail.com>
2019-07-17 08:59:52 -0400

    Assignmenting values on variable a with Assigment operators

--- chapter3/AssignmetOperators.java
+++ chapter3/AssignmetOperators.java
@@ -1,7 +1,43 @@
 package com.ocaj.exam.tutorial.chapter3;
 
 public class AssignmetOperators {
-    byte a =10;
+
+    public static void main(String[] args) {
+
+        byte a =10;
+        System.out.println("initial a valuer: "+a);
+        a =10;
+        a +=3;
+        System.out.println("a += 3: "+a);
+        a =15;
+        a -=3;
+        System.out.println("a -= 3: "+a);
+        a =20;
+        a *=3;
+        System.out.println("a *= 3: "+a);
+        a =25;
+        a /=3;
+        System.out.println("a /= 3: "+a);
+        a =35;
+        a %=3;
+        System.out.println("a %= 3: "+a);
+        a =40;
+        a ^=3;
+        System.out.println("a ^= 3: "+a);
+        a =45;
+        a |=3;
+        System.out.println("a |= 3: "+a);
+        a =50;
+        a <<=3;
+        System.out.println("a <<= 3: "+a);
+        a =55;
+        a >>=3;
+        System.out.println("a >>= 3: "+a);
+        a =60;
+        a >>>=3;
+        System.out.println("a >>>= 3: "+a);
+
+    }
 
 
 }
