55fdad7a952302b571830f843e98599b60e8b905
cleyton27 <cleyton27mb@gmail.com>
2019-07-17 09:18:23 -0400

    Basic Artihmetic Operators

---
+++ chapter3/BasicArithmeticOperators.java
@@ -0,0 +1,35 @@
+package com.ocaj.exam.tutorial.chapter3;
+
+public class BasicArithmeticOperators {
+
+    public static void main(String[] args) {
+        /*Addition operator example
+
+         */
+
+        int apples = 50;
+        int grennApples = 50;
+        int totalApples = apples + grennApples;
+        System.out.println("total of Apples: "+ totalApples);
+
+        // Substraction operator example
+        int spoliedApples = 10;
+        int totalGoogApples= totalApples- spoliedApples;
+        System.out.println("total of Apples: "+totalApples);
+
+        //Multiplication operator example
+        int applebox = 20;
+        int totalApplesInGeneral = totalApples * applebox;
+        System.out.println("total of Apples: "+totalApples);
+
+        // Division operator exemple
+        int fruitStores = 4;
+        int totalApplesInEachFruitStores = totalApplesInGeneral/ fruitStores;
+        System.out.println("total of apples per store: "+totalApplesInEachFruitStores);
+
+        // Modulus operator examplo
+
+        int applesRemainder = totalApples % fruitStores;
+        System.out.println("apples remainder "+ applesRemainder);
+    }
+}
