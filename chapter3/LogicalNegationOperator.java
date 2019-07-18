bce3dbe8e6e40097673030d9f038894e740f2257
cleyton27 <cleyton27mb@gmail.com>
2019-07-18 07:54:10 -0400

    The negation operetor ! invert the boolean values

---
+++ chapter3/LogicalNegationOperator.java
@@ -0,0 +1,18 @@
+package com.ocaj.exam.tutorial.chapter3;
+
+public class LogicalNegationOperator {
+    public static void main(String[] args) {
+        //Negation operator invert the value as the example bellow
+
+        // Prints true
+        System.out.println(!false);
+        // Prints false
+        System.out.println(!true);
+        // Prints true
+        System.out.println(!!true);
+        // Prints false
+        System.out.println(!!!true);
+        // Prints true
+        System.out.println(!!!!true);
+    }
+}
