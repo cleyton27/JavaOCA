6a6997c5244aebc26dd872df1a2f4dc535338504
cleyton27 <cleyton27mb@gmail.com>
2019-07-18 10:10:56 -0400

    Concatenation Operators

---
+++ chapter3/operators/strings/StringConcatenationOperator.java
@@ -0,0 +1,21 @@
+package com.ocaj.exam.tutorial.chapter3.operators.strings;
+
+public class StringConcatenationOperator {
+    public static void main(String[] args) {
+        String item = "boubloon";
+        String question = "Whati is a "+ item+"?";
+        System.out.println("Question: "+question);
+
+        //Concatenation with primitives
+        float reale = .007812f;
+        float escudo = .125f;
+        // value added
+        System.out.println(reale+ escudo+ " %percent of one gold doubloon");
+        // includes parentheses
+        System.out.println((reale+ escudo)+ " %percent of one gold doubloon");
+        // includes parentheses
+        System.out.println(" % percent of one gold doubloon "+(reale+ escudo) );
+        // values not added
+        System.out.println(" % percent of one gold doubloon "+reale+ escudo);
+    }
+}
