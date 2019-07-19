7205b29d80026958c29bec7931ceeb2703e51f05
cleyton27 <cleyton27mb@gmail.com>
2019-07-19 08:49:41 -0400

    Adding StringTest class

---
+++ chapter3/strings/StringTest.java
@@ -0,0 +1,16 @@
+package com.ocaj.exam.tutorial.chapter3.strings;
+
+public class StringTest {
+    public static void main(String[] args) {
+        /* Using a constructor to initialize a string variable isn't a good way to do it
+        because this consume a more memory
+        */
+        //String s1 = new String("String one");
+        // this way is more common and more indicate to initialize a string variable
+        String s1 = "String one";
+        String s2 = "String two";
+        String s3 = "String "+ "three";
+
+    }
+
+}
