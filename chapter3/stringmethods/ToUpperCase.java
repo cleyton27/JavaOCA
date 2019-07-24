e102f6f02cf77d147ffd053c04f11ed492755da1
cleyton27 <cleyton27mb@gmail.com>
2019-07-24 09:52:14 -0400

    the method toUpperCase returns the entire string as uppercase character

---
+++ chapter3/stringmethods/ToUpperCase.java
@@ -0,0 +1,12 @@
+package com.ocaj.exam.tutorial.chapter3.stringmethods;
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+        String pirateMessage = " Buried Treasure Chest! ";
+
+        // this method returns the entire string as uppercase character
+        String uc =pirateMessage.toUpperCase();
+        System.out.println(uc);
+
+    }
+}
