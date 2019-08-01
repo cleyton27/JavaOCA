84c40cc71827b0c3fa6fbeae4b712222ae7e24b2
cleyton27 <cleyton27mb@gmail.com>
2019-08-01 10:05:04 -0400

    Objects are a more advance data type than primitives"

---
+++ chapter4/primitives/objects/Car.java
@@ -0,0 +1,34 @@
+package com.ocaj.exam.tutorial.chapter4.primitives.objects;
+
+public class Car {
+    int topSpeed;
+    boolean running;
+
+    public Car(int topSpeed, boolean running) {
+        this.topSpeed = topSpeed;
+        this.running = running;
+    }
+
+    public boolean isRuning(){
+        return running;
+    }
+
+    /*
+    An object is declared in the same manner as primitive
+    but it cannot be used until it has been initialized
+     */
+
+    public static void main(String[] args) {
+        Car bigCar =new Car(125, true);
+        boolean running = bigCar.isRuning();
+
+        Car oldCar;
+        /*
+        This is an illegal example. You cannot use a method on an
+        uninitialized object
+         */
+        //boolean runningOldCar = oldCar.isRuning();
+
+
+    }
+}
