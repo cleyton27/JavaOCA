abc2e0948695eac67d11fecc0ba7c4ad8ef55e79
cleyton27 <cleyton27mb@gmail.com>
2019-08-01 10:46:10 -0400

    Is a good pratice check if the object is null.

--- chapter4/primitives/objects/Truck.java
+++ chapter4/primitives/objects/Truck.java
@@ -6,7 +6,10 @@ public class Truck {
     //Here is a example of how  check whether an object is null
     public static void main(String[] args) {
         Truck truck = null;
-        //
+        /*
+        The best way to shun a NullPointerException is initialize your objects
+        and use a conditional to check that may have been set to nul elsewhere in code.
+         */
         if(truck == null){
             System.out.println("truck is null");
         }else
