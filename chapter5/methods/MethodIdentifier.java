74a5de40d7fb55fe4c8ec016354a6c8ebb8a12c8
cleyton27 <cleyton27mb@gmail.com>
2019-08-12 08:38:27 -0400

    Method Identifier is the nmethod name

---
+++ chapter5/methods/MethodIdentifier.java
@@ -0,0 +1,13 @@
+package com.ocaj.exam.tutorial.chapter5.methods;
+
+public class MethodIdentifier {
+/*
+// Method Identifier sometimes called method name, is the text that is used to
+reference the method later in code.
+ */
+
+    //The method Identifier is declared after return type ex: sampleMethod();
+    public void sampleMethod(){
+
+    }
+}
