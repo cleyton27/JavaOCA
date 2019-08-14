3a9b32d1c0cba147a0201e5a467ae69ec6a34362
cleyton27 <cleyton27mb@gmail.com>
2019-08-14 10:48:52 -0400

    Local variable are declared inside of methods. As the name implies, these variables are use locally in code.

---
+++ chapter5/methods/localvariables/LocalVariables.java
@@ -0,0 +1,50 @@
+package com.ocaj.exam.tutorial.chapter5.methods.localvariables;
+
+public class LocalVariables {
+
+    void simpleMethod(){
+
+        //Starts of code A
+        int totatlCount =0;
+        System.out.println("Starting in A block: "+totatlCount);
+        //Starts of code B
+
+        for (int i = 0; i <3; i++) {
+            int forCount = 0;
+            totatlCount++;
+            forCount++;
+            System.out.println("Inside B block: "+totatlCount);
+            { //Starts of code C
+                int block1Count = 0;
+                totatlCount++;
+                forCount++;
+                block1Count++;
+                System.out.println("Inside C block: "+totatlCount);
+            }// Ends of bloc C
+
+            {// Stars block D
+                int block2Count = 0;
+                totatlCount++;
+                forCount++;
+                block2Count++;
+                System.out.println("Inside D block: "+totatlCount);
+            }//Ends of block D
+
+            /*
+            These two variables have no relation to above ones of the same name
+             */
+            int block1Count;
+            int block2Count;
+
+        } // Ends  of code block B
+
+        System.out.println("Final value of totalCount variable: "+totatlCount);
+    } // Ends of code clock A
+
+
+    public static void main(String[] args) {
+
+        LocalVariables lv = new LocalVariables();
+        lv.simpleMethod();
+    }
+}
