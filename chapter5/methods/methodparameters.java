828d9315ccec888ef42c795cf490013ed4439d22
cleyton27 <cleyton27mb@gmail.com>
2019-08-15 10:41:09 -0400

    The method paramets are the variable that are passed to the method from calling segment of code

---
+++ chapter5/methods/methodparameters.java
@@ -0,0 +1,17 @@
+package com.ocaj.exam.tutorial.chapter5.methods;
+
+public class methodparameters {
+    /*
+    The method parameters are the variable that are passed to the method from calling
+    segment of code. They are passed as argument to the method. Method parameters may be
+    primitives or objects.
+     */
+
+    float findMilesPerHour(float milesTraveled, float hoursTraveld){
+        return milesTraveled / hoursTraveld;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
