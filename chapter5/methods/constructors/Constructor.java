ef99c22fdb8e9bab4b76b8a9b91aa95e9deecdc9
cleyton27 <cleyton27mb@gmail.com>
2019-08-16 11:12:43 -0400

    The constructor is a special method that is used to initialize an object

---
+++ chapter5/methods/constructors/Constructor.java
@@ -0,0 +1,13 @@
+package com.ocaj.exam.tutorial.chapter5.methods.constructors;
+
+public class Constructor {
+
+    /*
+    The constructor is a special method that is used to initialize an object.
+    The constructor is called after new operator.
+     */
+
+    Integer intObject = new Integer(10);
+
+
+}
---
+++ chapter5/methods/constructors/LoanDetails.java
@@ -0,0 +1,45 @@
+package com.ocaj.exam.tutorial.chapter5.methods.constructors;
+
+public class LoanDetails {
+
+    private int term;
+    private double rate;
+    private double principal;
+
+    public LoanDetails() {
+        term =180;
+        rate = .0265;
+        principal= 0;
+    }
+
+    /*
+    Overloading constructor
+     */
+    public LoanDetails(int term, double rate, double principal) {
+        this.term = term;
+        this.rate = rate;
+        this.principal = principal;
+    }
+
+    public void setLoanDetails(double p) {
+        principal = p;
+    }
+
+    public double monthlyPayment(){
+        return (rate * principal / 12)/ (1.0 -Math.pow(((rate/12)+1.0), (-term)));
+    }
+
+    public static void main(String[] args) {
+        LoanDetails ld = new LoanDetails();
+        LoanDetails ld2 = new LoanDetails(10, .025,125000);
+        ld.setLoanDetails(15000);
+
+        System.out.println("Payment 1 : "+ld.monthlyPayment());
+        System.out.println("Payment 2 : "+ld2.monthlyPayment());
+
+
+
+
+    }
+
+}
