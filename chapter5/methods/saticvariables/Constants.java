839496541909a8ec0b77b734d5bc02677b650406
cleyton27 <cleyton27mb@gmail.com>
2019-08-21 10:16:10 -0400

    Constants, how to declare and how they works

---
+++ chapter5/methods/saticvariables/Constants.java
@@ -0,0 +1,17 @@
+package com.ocaj.exam.tutorial.chapter5.methods.saticvariables;
+
+public class Constants {
+
+/*
+Static variables can also be used to create constants in code. A constant is simply a variable that
+has a set value at compile time that can never be changed.
+A java constant is made by adding the final keyword to a class variable. The final keyword
+simply indicates that this variable can never have its value changed.
+ */
+
+
+// The name of constants should be all uppercase with words separated by underscores
+public static final double PI = 3.14;
+
+
+}
